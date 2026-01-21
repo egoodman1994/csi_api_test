d253f7353d2d8059a0349ed3c134bd3109224756
egoodman1994 <48113148+egoodman1994@users.noreply.github.com>
2026-01-21 09:43:48 -0500

    Remove comments and authorization header from config

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
-    // All requests we send go to this API endpoint.
     baseURL: 'https://fakestoreapi.com',
     extraHTTPHeaders: {
       'Accept': 'application/json',
-      // 'Authorization': `token ${process.env.API_TOKEN}`,
     },
   }
 });
